6384175f8dd8952c7a358cff9f964a0d920d0c22
georgejincy <jincydon101@gmail.com>
2017-02-25 11:57:51 -0500

    Add logic to have two handle requests each for one server

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 var http = require('http');
 
 var PORT1 = 7000;
-var PORT1 = 7500;
+var PORT2 = 7500;
 
 //generic for handling requests
 function handleRequest(request, response){
-	response.end("It works on path " + request.url);
+	response.end("You are awesome " + request.url);
+
+}
+function handleRequest2(request, response){
+	response.end("You are bad! " + request.url);
+	
 }
 
 var server1 = http.createServer(handleRequest);
-var server2 = http.createServer(handleRequest);
+var server2 = http.createServer(handleRequest2);
 
 server1.listen(PORT1, function(){
 	console.log("Server is listening on http://localhost: %s", PORT1);
 });
 
-server2.listen(PORT1, function(){
+server2.listen(PORT2, function(){
 	console.log("Server is listening on http://localhost: %s", PORT2);
 });
